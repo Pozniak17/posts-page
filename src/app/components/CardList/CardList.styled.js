54182dcf6f8c16daa4be4d601701dd6919a3d748
Yevhenii Pozniak <jekamanu@gmail.com>
2024-06-19 14:37:11 +0300

    Update CardList.styled.js

--- src/app/components/CardList/CardList.styled.js
+++ src/app/components/CardList/CardList.styled.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
 export const List = styled.ul`
-  margin: 5px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  justify-items: center;
 `;
